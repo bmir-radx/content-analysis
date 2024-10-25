978afb1ca6e1a96be4f34582d95daa438261728c
Marcos Martinez-Romero <marcosromero@h6pyhk9jhf.hsd1.ca.comcast.net>
2024-10-24 21:41:08 -0700

    Rename scripts

--- content_analysis.R
+++ content_analysis.R
@@ -10,9 +10,15 @@ library(viridis)
 
 base_path <- "/Users/marcosromero/Projects/2024-10-RADxDataHub-ContentReport/"
 
-publications_file_path <- paste0(base_path, "files/RADx_Data_Hub-List_of_Publications.xlsx")
 study_metadata_file_path <- paste0(base_path, "files/RADx_Study_Metadata_10232024xlsx.xlsx")
 study_domainpopfocus_file_path <- paste0(base_path, "files/Study Domain and Population Focus Consolitation.xlsx")
+
+output_dir <- paste0(base_path, "plots/") 
+
+# Ensure the output directory exists
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
 
 palette1 <- c(
   "#FF5733", "#1E88E5", "#689F38", "#D81B60", "#3949AB", "#6A1B9A", "#00897B", "#546E7A", 
@@ -45,15 +51,44 @@ palette2 <- c(
   "#455A64", "#E53935", "#4CAF50", "#FB8C00", "#FF5733", "#E91E63", "#616161", "#9C27B0"
 )
 
-palettes <- list(palette1, palette2)
+palette3 <- c(
+  "#A58C6F",  # Light Brown
+  "#7A6174",  # Muted Purple
+  "#8A6F56",  # Brown
+  "#6B737B",  # Muted Blue Gray
+  "#AFB68F",  # Muted Green
+  "#8D6B94",  # Muted Purple
+  "#A49379",  # Light Beige
+  "#7D8E7A",  # Muted Forest Green
+  "#799F99",  # Muted Green Blue
+  "#AC8471",  # Muted Reddish Brown
+  "#918471",  # Grayish Brown
+  "#998C7B",  # Taupe Brown
+  "#9D9381",  # Earthy Beige
+  "#8F8D79",  # Olive Gray
+  "#A66E7A",  # Muted Reddish Purple
+  "#647A68",  # Muted Greenish Gray
+  "#75617E",  # Muted Purple
+  "#6A5D57",  # Earthy Gray Brown
+  "#9B786F",  # Earthy Pinkish Brown
+  "#6C7074",  # Muted Blue-Gray
+  
+  # Additional colors that complement the earthy palette
+  "#6B4226", "#5D4037", "#7F462C", "#845422", "#8E460E", "#986D4F", "#A08058", "#9C8466",
+  "#B07D62", "#7C614A", "#7E481B", "#6D3D1B", "#A98464", "#BC8F8F", "#CD853F", "#8B4513",
+  "#8FBC8F", "#556B2F", "#6B8E23", "#808000", "#2F4F4F", "#008080", "#2E8B57", "#3CB371",
+  "#5F6A6A", "#4D5656", "#6E7F80", "#4E4E4E", "#515A5A", "#424949", "#1C2833", "#1E4D2B",
+  "#324D4F", "#4D644F", "#6B4226", "#7F7F7F", "#9C9C9C", "#7C756A", "#7B7D7D", "#AD743C",
+  "#9F8C63", "#9B5513", "#B87333", "#C9AE82", "#846544", "#897665", "#8B7E6B", "#A52A2A",
+  "#BA4A00", "#873600", "#DAA520", "#B8860B", "#D2691E", "#DEB887", "#D2B48C", "#8B0000",
+  "#B22222", "#800000", "#CD5C5C", "#FF4500", "#D35400", "#E59866", "#D98880", "#FF6347",
+  "#FF8C00", "#FF7F50", "#FFD700", "#F39C12", "#E67E22", "#F4A460", "#FA8072", "#E9967A",
+  "#F08080", "#B03A2E", "#C0392B", "#922B21", "#7B241C", "#943126", "#78281F", "#641E16")
+
+
+palettes <- list(palette1, palette2, palette3)
 
 ### FUNCTIONS
-
-# Function to extract the year from the publication date
-extract_year <- function(pub_date) {
-  year <- as.numeric(strsplit(pub_date, " ")[[1]][1])
-  return(year)
-}
 
 # Function to standardize a column
 standardize_column <- function(df, column_name) {
@@ -64,7 +99,7 @@ standardize_column <- function(df, column_name) {
 }
 
 # Function to generate all chart types
-generate_all_charts <- function(data, columns, id_column = "PMID", title = "Charts of Publications", min_count = 1, custom_palettes = NULL) {
+generate_all_charts <- function(data, columns, id_column = "PMID", title = "Charts", min_count = 1, custom_palettes = NULL) {
   
   # Combine columns if multiple are provided
   if (length(columns) > 1) {
@@ -87,41 +122,66 @@ generate_all_charts <- function(data, columns, id_column = "PMID", title = "Char
   # Group by the combined column and count distinct IDs
   chart_data <- data_cleaned %>%
     group_by(combined_column) %>%
-    summarize(publication_count = n_distinct(!!sym(id_column))) %>%
-    filter(publication_count >= min_count)
+    summarize(record_count = n_distinct(!!sym(id_column))) %>%
+    filter(record_count >= min_count)
   
   # List of chart types to generate
-  chart_types <- c("treemap", "bar", "bubble")
+  chart_types <- c("treemap", "bar")
   
   # Helper function to generate and print charts
   generate_plot <- function(custom_palette, title_suffix, chart_type) {
     plot <- NULL
+    plot_title <- paste(title, title_suffix)
     
+    # Generate different types of plots based on chart_type
     if (chart_type == "treemap") {
-      plot <- ggplot(chart_data, aes(area = publication_count, fill = combined_column, label = combined_column)) +
+      plot <- ggplot(chart_data, aes(area = record_count, fill = combined_column, label = combined_column)) +
         geom_treemap() +
         geom_treemap_text(colour = "white", place = "centre", grow = TRUE, reflow = TRUE)
       
     } else if (chart_type == "bar") {
-      plot <- ggplot(chart_data, aes(x = reorder(combined_column, -publication_count), y = publication_count, fill = combined_column)) +
+      plot <- ggplot(chart_data %>% 
+                       arrange(desc(record_count)) %>% 
+                       slice_head(n = 20), 
+                     aes(x = reorder(combined_column, record_count), y = record_count, fill = combined_column)) +
         geom_bar(stat = "identity") +
-        coord_flip()
+        coord_flip() +
+        labs(y = "Study Count", x = "") +
+        theme(
+          axis.text.x = element_text(size = 14),  # Increase x-axis text size
+          axis.text.y = element_text(size = 16),  # Increase y-axis text size
+          axis.title.x = element_text(size = 16),  # Increase x-axis title size
+          axis.title.y = element_text(size = 16),  # Increase y-axis title size
+          plot.title = element_text(size = 18)  # Increase plot title size (if you have one)
+        )
       
     } else if (chart_type == "bubble") {
-      plot <- ggplot(chart_data, aes(x = combined_column, y = publication_count, size = publication_count, fill = combined_column)) +
+      plot <- ggplot(chart_data, aes(x = combined_column, y = record_count, size = record_count, fill = combined_column)) +
         geom_point(alpha = 0.7, shape = 21, colour = "black") +
         theme_minimal()
-      
     }
     
+    # Apply custom palette if provided
     if (!is.null(custom_palette)) {
       plot <- plot + scale_fill_manual(values = custom_palette)
     }
     
-    plot <- plot + labs(title = paste(title, title_suffix)) +
+    # Add title and theme settings
+    plot <- plot + labs(title = plot_title) +
       theme(legend.position = "none", plot.margin = unit(c(1, 1, 1, 1), "cm"))
     
+    # Display the plot
     print(plot)
+    
+    # Define a file name based on the plot type, title, and palette
+    file_name <- paste0(output_dir, gsub(" ", "_", plot_title), "_", chart_type, "10x6.png")
+    # Save the plot to a file
+    ggsave(file_name, plot = plot, width = 10, height = 6, dpi = 300)
+    
+    # Define a file name based on the plot type, title, and palette
+    file_name <- paste0(output_dir, gsub(" ", "_", plot_title), "_", chart_type, "12x10.png")
+    # Save the plot to a file
+    ggsave(file_name, plot = plot, width = 12, height = 10, dpi = 300)
   }
   
   # Generate all chart types (no custom palette)
@@ -144,36 +204,20 @@ generate_all_charts <- function(data, columns, id_column = "PMID", title = "Char
 #### PREPROCESSING ###
 
 # Load the data 
-publications_df <- read_excel(publications_file_path)
 study_df <- read_excel(study_metadata_file_path, sheet = "Cleaned Metadata")
 study_dpf_df <- read_excel(study_domainpopfocus_file_path)
 
 # Rename the columns so that they have the same name for merging
-publications_df <- publications_df %>% rename(dbgap_accession = "dbGaP Accession")
 study_df <- study_df %>% rename(dbgap_accession = PHS)
 study_dpf_df <- study_dpf_df %>% rename(dbgap_accession = PHS)
 
 # Remove rows where dbgap_accession is NA
-publications_df <- publications_df %>% filter(!is.na(dbgap_accession))
 study_df <- study_df %>% filter(!is.na(dbgap_accession))
 study_dpf_df <- study_dpf_df %>% filter(!is.na(dbgap_accession))
 
 # Apply normalization to both dataframes
-publications_df <- standardize_column(publications_df, "dbgap_accession")
 study_df <- standardize_column(study_df, "dbgap_accession")
 study_dpf_df <- standardize_column(study_dpf_df, "dbgap_accession")
-
-# Perform the merge
-merged_df <- publications_df %>%
-  left_join(study_df, by = "dbgap_accession")
-
-# Apply the function to add the 'Publication Year' column
-merged_df <- merged_df %>%
-  mutate(Publication_Year = sapply(`Publication Date`, extract_year))
-
-# Export merged_df to a CSV file
-output_csv_file <- paste0(base_path, "files/merged_df.csv")
-write.csv(merged_df, file = output_csv_file, row.names = FALSE)
 
 ### STUDY CHARTS
 
@@ -189,112 +233,3 @@ generate_all_charts(radx_up_df, c("Refined population"), id_column = "dbgap_acce
 generate_all_charts(radx_rad_df, c("refined topic"), id_column = "dbgap_accession", title = "RADx-rad Studies by Topic", min_count = 3, custom_palettes = palettes)
 generate_all_charts(radx_tech_df, c("refined topic"), id_column = "dbgap_accession", title = "RADx-Tech Studies by Topic", min_count = 1, custom_palettes = palettes)
 
-
-
-# # TreeMaps
-# generate_all_charts(merged_df, c("source", "source_other_specify"), title = "Treemap of Publications by Study Source", min_count = 3, custom_palettes=palettes)
-# generate_all_charts(merged_df, c("Study_Population_Focus"), title = "Treemap of Publications by Study Population Focus")
-# generate_all_charts(merged_df, c("data_general_types", "data_general_types_other_specify"), title = "Treemap of Publications by Data Type", min_count = 5)
-# generate_all_charts(merged_df, c("topics"), title = "Treemap of Publications by Study Topics")
-# generate_all_charts(merged_df, c("types"), title = "Treemap of Publications by Study Type")
-# generate_all_charts(merged_df, c("institutes_supporting_study"), title = "Treemap of Publications by Institute")
-# generate_all_charts(merged_df, c("subject"), title = "Treemap of Publications by Subject", min_count = 7)
-# generate_all_charts(merged_df, c("Journal"), title = "Treemap of Publications by Journal", min_count = 2)
-# 
-# # Bar Chart: Number of Publications per year
-# ggplot(merged_df, aes(x = as.factor(Publication_Year))) +
-#   geom_bar(fill = "lightblue") +
-#   scale_x_discrete(limits = as.character(2020:format(Sys.Date(), "%Y"))) +  # Set x-axis to start from 2020 onwards
-#   labs(title = "Publications Per Year", x = "Year", y = "Number of Publications") +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels if needed
-# 
-# # Number of publications per dataset
-# # Group by 'dbgap_accession' (dataset) and count distinct publications (PMID)
-# dataset_publications <- merged_df %>%
-#   filter(!is.na(dbgap_accession), !is.na(PMID)) %>%  # Ensure no missing values
-#   group_by(dbgap_accession) %>%
-#   summarize(publication_count = n_distinct(PMID)) %>%
-#   arrange(desc(publication_count))  # Sort by publication count in descending order
-# 
-# # Select the top 20 datasets based on publication count
-# top_datasets <- dataset_publications %>%
-#   top_n(20, wt = publication_count)
-# 
-# # Create a bar chart showing the top datasets
-# ggplot(top_datasets, aes(x = reorder(dbgap_accession, -publication_count), y = publication_count)) +
-#   geom_bar(stat = "identity", fill = "steelblue") +
-#   labs(title = "Top 10 Datasets by Number of Publications", x = "Dataset (dbGaP Accession)", y = "Number of Publications") +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for better readability
-# 
-# 
-# 
-# ### Heatmap: number of publications by topic by year
-# heatmap_data <- treemap_topics_data %>%
-#   count(combined_column, Publication_Year) %>%
-#   spread(Publication_Year, n, fill = 0)
-# 
-# # Pivot longer to get back the 'Publication_Year' column for heatmap plotting
-# heatmap_data_long <- heatmap_data %>%
-#   pivot_longer(cols = starts_with("20"),  # Assumes years are 20XX, adjust if needed
-#                names_to = "Publication_Year",
-#                values_to = "n")
-# 
-# # Limit to the top 10 topics by publication count and recent years (e.g., 2020 onwards)
-# top_topics <- treemap_topics_data %>%
-#   count(combined_column, sort = TRUE) %>%
-#   top_n(10, wt = n)  # Top 10 topics by publication count
-# 
-# heatmap_data_filtered <- heatmap_data_long %>%
-#   filter(combined_column %in% top_topics$combined_column, Publication_Year >= 2020)
-# 
-# # Now plot the heatmap with filtered data
-# ggplot(heatmap_data_filtered, aes(x = Publication_Year, y = combined_column, fill = n)) +
-#   geom_tile(color = "white") +
-#   scale_fill_gradient(low = "lightblue", high = "darkblue") +
-#   labs(title = "Heatmap of Publications by Topic and Year", x = "Publication Year", y = "Topics") +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for better readability
-# 
-# 
-
-
-
-### TRASH
-
-# # Step 1: Split the 'subject' column by semicolon into multiple rows
-# merged_df_split <- merged_df %>%
-#   filter(!is.na(subject)) %>%  # Remove rows with missing subjects
-#   separate_rows(subject, sep = ";") %>%  # Split the 'subject' column by semicolon
-#   mutate(subject = str_trim(subject))  # Trim any leading/trailing spaces
-# 
-# # Step 2: Group data by 'subject' and count the number of distinct publications (PMID) and datasets (dbgap_accession)
-# subject_data <- merged_df_split %>%
-#   filter(!is.na(PMID), !is.na(dbgap_accession)) %>%  # Filter out missing values
-#   group_by(subject) %>%
-#   summarize(publication_count = n_distinct(PMID),
-#             dataset_count = n_distinct(dbgap_accession)) %>%
-#   filter(dataset_count > 0)  # Remove any subjects without datasets
-# 
-# # Step 3: Calculate the ratio of publications to datasets
-# subject_data <- subject_data %>%
-#   mutate(publication_to_dataset_ratio = publication_count / dataset_count)
-# 
-# # Step 4: Identify subjects with a high ratio of publications to datasets
-# # (This highlights subjects with many publications but few datasets)
-# subject_data_high_ratio <- subject_data %>%
-#   filter(publication_to_dataset_ratio > 1)  # Adjust the threshold if needed
-# 
-# # Step 5: Limit to the top 50 subjects based on publication-to-dataset ratio
-# subject_data_top_50 <- subject_data_high_ratio %>%
-#   top_n(50, wt = publication_to_dataset_ratio)
-# 
-# # Step 6: Generate a bar chart showing the ratio of publications to datasets for the top 50 subjects
-# ggplot(subject_data_top_50, aes(x = reorder(subject, -publication_to_dataset_ratio), y = publication_to_dataset_ratio)) +
-#   geom_bar(stat = "identity", fill = "steelblue") +
-#   labs(title = "Top 50 Subjects by Ratio of Publications to Datasets",
-#        x = "Subject",
-#        y = "Publications to Datasets Ratio") +
-#   theme_minimal() +
-#   theme(axis.text.x = element_text(angle = 80, hjust = 1))  # Rotate x-axis labels for better readability
